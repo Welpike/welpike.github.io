1553f964360d130cf753331fdcd826d7a572e6f4
Welpike <106405084+Welpike@users.noreply.github.com>
2022-09-01 15:08:53 +0200

    Update js-redirect.js

--- api/js-redirect.js
+++ api/js-redirect.js
@@ -1,2 +1,2 @@
 // Welpike - welpike.github.io
-var param = location.search.substring(1);if(param == ""){param = encodeURIComponent("https://welpike.github.io");}var url = param.substring(param.indexOf("=")+1, param.length);var delay = 2;url = decodeURIComponent(url);setTimeout("document.location.replace(url)", delay + '000');
+var param = location.search.substring(1);if(param == ""){param = encodeURIComponent("https://welpike.github.io");}var url = param.substring(param.indexOf("=")+1, param.length);var delay = 2;url = decodeURIComponent(url);setTimeout("window.location.replace(url)", delay + '000');
