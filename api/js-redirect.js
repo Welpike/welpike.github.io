86b253d7f6af355969245b2e2deccdd2c550ffd3
Welpike <106405084+Welpike@users.noreply.github.com>
2023-03-18 21:44:48 +0100

    migration of the website -> @Welpike/url

--- api/js-redirect.js
+++
@@ -1,2 +0,0 @@
-// Welpike - welpike.github.io
-var param = location.search.substring(1);if(param == ""){param = encodeURIComponent("https://welpike.github.io");}var url = param.substring(param.indexOf("=")+1, param.length);var delay = 2;url = decodeURIComponent(url);setTimeout("window.location.replace(url)", delay + '000');
--- api/redirect.js
+++
@@ -1,2 +0,0 @@
-// Welpike - welpike.github.io
-function redirect(url,delay){url=new URL(url);setTimeout("window.location.replace(url)", delay + '000');}
